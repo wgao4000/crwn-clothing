9cd38380b29494cf51e2c5f2d254a01b71e00e7b
wgao4000 <wgao4000@gmail.com>
2021-05-25 13:15:01 -0400

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const shopPage = () => (<h1>Shopping Page</h1>);
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component = {HomePage} />
+        <Route exact path="/shop" component = {shopPage} />
+      </Switch>
     </div>
   );
 }
